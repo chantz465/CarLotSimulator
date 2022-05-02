4eb71d8d88f093d240e76c37b4f6556c639679e2
Chance Gardner <Chantz465@gmail.com>
2022-05-02 11:32:38 -0500

    (first add of exercise1)

---
+++ Exercise1/Exercise1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercise1
+{
+    public class Car
+    { 
+        public int Year { get; set; }
+        public string Make { get; set; }
+
+        public string model { get; set; }
+
+        public void CarOut(int yearMade, string madeBy, string modelOf)
+        {
+            Console.WriteLine($"My car is a {yearMade} {madeBy} {modelOf} and I love it!");
+        
+        }
+    
+    
+    
+    
+    }
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            car.Year = 2005;
+            car.Make = "Bmw";
+            car.model = "F650gs";
+
+            car.CarOut(2005,"bmw","f650gs");
+
+
+        }
+    }
+}
